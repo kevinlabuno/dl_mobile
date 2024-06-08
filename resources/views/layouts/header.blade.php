beabcf41a6d77b9e5a446f1094911d6aaa18ab5a
kevinlabuno <kevinlabuno35@gmail.com>
2024-06-08 20:22:32 +0800

    menu change

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,9 @@
 <div class="header-navbar">
     <div class="scrollable-nav">
-        <a href="{{url('/')}}"><i class="fas fa-home"></i> Home</a>
-        <a href="#page1"><i class="fas fa-file"></i> Page 1</a>
-        <a href="#page2"><i class="fas fa-file-alt"></i> Page 2</a>
-        <a href="#page3"><i class="fas fa-file-archive"></i> Page 3</a>
-        <a href="#page4"><i class="fas fa-file-code"></i> Page 4</a>
+        <a href="#page1"><i class="fas fa-file"></i>Omset</a>
+        <a href="#page2"><i class="fas fa-file-alt"></i>Pengeluaran</a>
+        <a href="#page3"><i class="fas fa-file-archive"></i>Pengambilan</a>
+        <a href="#page4"><i class="fas fa-file-code"></i>Pembayaran</a>
         <a href="#page5"><i class="fas fa-file-image"></i> Page 5</a>
         <a href="#page6"><i class="fas fa-file-pdf"></i> Page 6</a>
     </div>
